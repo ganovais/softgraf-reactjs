ce9b56f6394b13749a29b3b77c59c2699bb0167f
Gabriel Novais <ganovais@novecom.local>
2022-05-03 19:15:39 -0300

    Introdução a API usando Node.js com express

--- src/styles/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import "styled-components";
-import { theme_dark } from "./theme_dark";
-
-declare module "styled-components" {
-   type ThemeType = typeof theme_dark;
-
-   export interface DefaultTheme extends ThemeType {}
-}
